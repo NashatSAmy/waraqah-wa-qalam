1837092bb3e0a6ece51eec9419112cbee5e695bd
Nashat Samy <nashat.masamy@gmail.com>
2023-06-17 14:19:25 +0300

    Task edit implemented UI style changes

--- src/taskListModule.js
+++ src/taskListModule.js
@@ -6,6 +6,10 @@ Object.defineProperty(String.prototype, "capitalize", {
   enumerable: false,
 });
 
+function noDefault(e) {
+  e.preventDefault()
+}
+
 ////////////////////////////////////////
 //////////Tasks List Objects///////////
 //////////////////////////////////////
@@ -284,29 +288,32 @@ const taskView = {
     const mask = document.createElement("div");
     mask.classList.add("blackMask");
     mask.innerHTML = `
-      <div class="taskView">
+      <div class="taskView" data-taskid="${item.idNo}">
        <div class="taskView-startDate">${item.startingDate}</div>
-       <div class="taskView-group">
-          <div id="taskEdit"></div>
+       <div class="taskView-group" data-editable="choose">
+          <div id="taskEdit" title="Edit Task"></div>
           ${item.group}
        </div>
-       <div class="taskView-name">${item.name.capitalize()}</div>
-       <div class="taskView-dueDate">
+       <div class="taskView-name" data-editable="rewrite">${item.name.capitalize()}</div>
+       <div class="taskView-dueDate" data-editable="choose">
          Due: ${new Date(item.dueDate).toDateString()} ${new Date(
       item.dueDate
     ).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
        </div>
-       <div class="taskView-priority">Priority: ${item.priority}</div>
+       <div class="taskView-priority" data-editable="choose">Priority: ${item.priority}</div>
        <div class="taskView-etc">${item.ETC}</div>
        <div class="taskView-totalTime">${item.workTime}</div>
        <div class="taskView-description">
-         <span id="desName">Description:</span><span id="desText">${
+         <span id="desName">Description:</span><span id="desText" data-editable="rewrite">${
            item.description
          }</span>
        </div>
-       <div class="taskView-notes">
-         <span id="notesName">Notes:</span> 
+       <div id="taskView-notes">
+         <span id="notesName">Notes: <div title="Add Sub Task" id="addSubTasksTV"></div>
+         <div title="Add Sub Note" id="addSubNotesTV"></div></span> 
          </span>
+         <div id="taskView-notesTS"></div>
+         <div id="taskView-notesNS"></div>
        </div>
      </div>
     `;
@@ -314,7 +321,8 @@ const taskView = {
     taskView.makeSubItem(item.subItems);
   },
   makeSubItem(subItemsList) {
-    const listNode = document.getElementsByClassName("taskView-notes")[0];
+    const taskList = document.getElementById("taskView-notesTS");
+    const noteList = document.getElementById("taskView-notesNS");
     const validationTest = /\w/;
     subItemsList.forEach((subItem) => {
       if (
@@ -322,20 +330,165 @@ const taskView = {
         validationTest.test(subItem.value) == true
       ) {
         const taskNode = document.createElement("label");
-        taskNode.innerHTML = `<span>${subItem.value}</span> <input type="checkbox" name="" id="">`;
-        listNode.appendChild(taskNode);
+        taskNode.innerHTML = `<div class="removeTVSubItem"></div><span data-editable="editSubItem">${subItem.value}</span> <input type="checkbox" name="" id="">`;
+        taskList.appendChild(taskNode);
       } else if (
         subItem.type == "note" &&
         validationTest.test(subItem.value) == true
       ) {
         const noteNode = document.createElement("span");
         noteNode.classList.add("subNoteText");
-        noteNode.innerText = subItem.value;
-        listNode.appendChild(noteNode);
+        noteNode.setAttribute("data-editable", "editSubItem")
+        noteNode.innerHTML = `<div class="removeTVSubItem"></div>${subItem.value}`;
+        noteList.appendChild(noteNode);
       }
     });
   },
+  showNoteInput(e) {
+    // Function that allow the user to input new sub-item task or note
+    const notes = document.getElementById("taskView-notes");
+    if (e.target.id == "addSubTasksTV") {
+      const taskInput = document.createElement("div");
+      taskInput.setAttribute("id", "subTaskInputContainerTV")
+      taskInput.innerHTML = `<input type="text" id="subTaskInputTV"><button type="button" id="subTaskAddButtonTV">Add Task</button>`;
+      taskInput.addEventListener("focusout", () => setTimeout(() => {
+        document.getElementById("subTaskInputTV").value = "";
+        taskInput.remove()
+      }, 150))
+      notes.appendChild(taskInput);
+      document.getElementById("subTaskInputTV").focus();
+      document.getElementById("subTaskAddButtonTV").addEventListener("click", (e) => e.preventDefault());
+      taskView.postNote("subTaskInputTV")
+    } else if (e.target.id == "addSubNotesTV") {
+      const noteInput = document.createElement("div");
+      noteInput.setAttribute("id", "subTaskInputContainerTV")
+      noteInput.innerHTML = `<textarea id="subNoteInputTV" rows="4"></textarea> <button type="button" id="subNoteAddButtonTV">Add Note</button>`;
+      noteInput.addEventListener("focusout", () => setTimeout(() => {
+        document.getElementById("subNoteInputTV").value = ""
+        noteInput.remove()
+      }, 150));
+      notes.appendChild(noteInput);
+      document.getElementById("subNoteInputTV").focus()
+      taskView.postNote("subNoteInputTV")
+    }
+    return;
+  },
+  postNote(id){
+    // Function that post a note or a task to the DOM.
+    const taskList = document.getElementById("taskView-notesTS");
+    const noteList = document.getElementById("taskView-notesNS");
+    const subItemInput = document.getElementById(id);
+    window.addEventListener("click", (e) => {
+      if (e.target.id == "subTaskAddButtonTV" && subItemInput.value.trim() !== "") {
+        const taskNode = document.createElement("label");
+        taskNode.innerHTML = `<div class="removeTVSubItem"></div><span data-editable="editSubItem">${subItemInput.value}</span> <input type="checkbox" name="" id="">`;
+        taskList.appendChild(taskNode);
+      } else if (e.target.id == "subNoteAddButtonTV" && subItemInput.value.trim() !== "") {
+        const noteNode = document.createElement("span");
+        noteNode.classList.add("subNoteText");
+        noteNode.setAttribute("data-editable", "editSubItem")
+        noteNode.innerHTML = `<div class="removeTVSubItem"></div> ${subItemInput.value}`;
+        noteList.appendChild(noteNode);
+      }
+    })
+  },
+  deleteSubItem(e){
+    if (!e.target.classList.contains("removeTVSubItem")) return;
+    const parentContainer = e.target.parentNode;
+    const noteValue = {note: parentContainer.innerText, task: parentContainer.childNodes[1].innerText};
+    const noteType = parentContainer.childNodes.length;
+    const noteFile = {type: noteType == 4 ? "task" : "note", value: noteType == 4 ? noteValue.task : noteValue.note};
+    parentContainer.remove()
+  }
 };
+
+const taskEditing = {
+  taskEditGreenLight: false,
+  editableTaskIdNo: "",
+  enableEditing(e) {
+    if (e.target.id != "taskEdit") return;
+    taskEditing.taskEditGreenLight = true;
+    taskEditing.editableTaskIdNo = e.target.parentNode.parentNode.dataset.taskid;
+    // Save button
+    const saveChangesButton = document.createElement("button");
+    saveChangesButton.setAttribute("type", "button");
+    saveChangesButton.setAttribute("id", "saveChangesButton");
+    saveChangesButton.innerText = `Save`
+    document.getElementsByClassName("taskView")[0].appendChild(saveChangesButton);
+    // Getting all editable fields.
+    const rewritableFields = document.querySelectorAll("[data-editable='rewrite']");
+    const selectiveFields = document.querySelectorAll("[data-editable='choose']");
+    const subItems = document.querySelectorAll("[data-editable='editSubItem']");
+    const subItemsParents = document.querySelectorAll("div#taskView-notes label");
+    // Enabling editing for the selected fields.
+    rewritableFields.forEach(item => item.classList.add("rewriteMe"));
+    selectiveFields.forEach(item =>{
+      if (item.classList[0].split("-")[1] == "group") {
+        const newElement = document.createElement("select");
+        newElement.setAttribute("id", "editGroup")
+        const selectionList = []
+        const groupList = document.querySelectorAll(".project").forEach( group => group.innerText == item.innerText ? selectionList.push(`<option selected>${group.innerText}</option>`) : selectionList.push(`<option>${group.innerText}</option>`));
+        newElement.innerHTML = `${selectionList}`;
+        item.replaceWith(newElement)
+      }
+      else if (item.classList[0].split("-")[1] == "dueDate") {
+        const newElement = document.createElement("input");
+        newElement.setAttribute("type", "datetime-local");
+        newElement.setAttribute("id", "editDueDate")
+        item.replaceWith(newElement)
+      }
+      else if (item.classList[0].split("-")[1] == "priority") {
+        const newElement = document.createElement("select");
+        newElement.setAttribute("id", "editPriority")
+        const selectionList = ["urgent", "Important", "Not Important"]
+        newElement.innerHTML = `${selectionList.map(priority => item.innerText.split(":")[1].trim() == priority ? `<option selected>${priority}</option>` : `<option>${priority}</option>`)}`;
+        item.replaceWith(newElement)
+      }
+    });
+    subItems.forEach(item => item.classList.add("rewriteMe"));
+    subItemsParents.forEach(item => item.addEventListener("click", noDefault));
+  },
+  disableEditing(e) {
+    if (e.target.id != "saveChangesButton") return;
+    // Getting all editable fields.
+    const rewritableFields = document.querySelectorAll("[data-editable='rewrite']");
+    const subItems = document.querySelectorAll("[data-editable='editSubItem']");
+    const subItemsParents = document.querySelectorAll("div#taskView-notes label");
+    const group = document.getElementById("editGroup");
+    const dueDate = document.getElementById("editDueDate");
+    const priority = document.getElementById("editPriority");
+    // Disabling editing for the selected fields.
+    rewritableFields.forEach(item => item.classList.remove("rewriteMe"));
+    subItems.forEach(item => item.classList.remove("rewriteMe"));
+    subItemsParents.forEach(item => item.removeEventListener("click", noDefault));
+    // New taskView-group element
+    const ogElemG = document.createElement("div");
+    ogElemG.classList.add("taskView-group");
+    ogElemG.setAttribute("data-editable", "choose")
+    ogElemG.innerHTML = `<div id="taskEdit" title="Edit Task"></div> ${group.value}`
+    group.remove()
+    // New taskView-dueDate element
+    const ogElemDD = document.createElement("div");
+    ogElemDD.classList.add("taskView-dueDate");
+    ogElemDD.setAttribute("data-editable", "choose")
+    ogElemDD.innerHTML = `Due: ${new Date(dueDate.value).toDateString()} ${new Date(
+      dueDate.value
+    ).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}`
+    dueDate.remove()
+    // New taskView-priority element
+    const ogElemP = document.createElement("div");
+    ogElemP.classList.add("taskView-priority");
+    ogElemP.setAttribute("data-editable", "choose")
+    ogElemP.innerHTML = `Priority: ${priority.value}`
+    priority.remove()
+    // Appending all of the new elements
+    document.getElementsByClassName("taskView")[0].appendChild(ogElemG);
+    document.getElementsByClassName("taskView")[0].appendChild(ogElemDD);
+    document.getElementsByClassName("taskView")[0].appendChild(ogElemP);
+
+    e.target.remove()
+  }
+}
 
 /////////Tasks List Eventlisteners
 window.addEventListener("click", newTaskInfo.getAllInfo);
@@ -352,3 +505,9 @@ window.addEventListener("click", listItemController.shiftDomList);
 
 /////////Tasks View Eventlisteners
 window.addEventListener("click", taskView.expand);
+window.addEventListener("click", taskView.showNoteInput);
+window.addEventListener("click", taskView.deleteSubItem);
+
+/////////Tasks Editing Eventlisteners
+window.addEventListener("click", taskEditing.enableEditing);
+window.addEventListener("click", taskEditing.disableEditing)
